cd10e869f2fed24238302efd930662903ce66dd3
Vipin <vipintripathi121@gmail.com>
2020-05-19 11:03:22 +0530

    prepare to deploy

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-//server.js
 const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-app.listen(port);
+const port = process.env.PORT || 3000;
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'client/build')));
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));  }
+//Route
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+//Start
+app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
