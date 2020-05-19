cc04311c4a4fa8863727c554de489ca2951be3ec
Vipin <vipintripathi121@gmail.com>
2020-05-19 11:53:25 +0530

    delete package.lock.json

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'client/build')));
-if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));  }
-//Route
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
-//Start
-app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
+const port = process.env.PORT || 3002;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+app.listen(port, () => {
+   console.log(`Server is up on port ${port}!`);
+});
